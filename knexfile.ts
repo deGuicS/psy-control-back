e903c1776d68a644cfb7fcaaa5b3fc5844724454
deGuicS <guisilvaeco@gmail.com>
2025-01-19 16:37:22 -0300

    chore: adding ping to see if db is running, still not working

--- knexfile.ts
+++ knexfile.ts
@@ -1,12 +1,11 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
 import * as dotenv from "dotenv";
-import fs from "fs";
+import * as path from "path";
+import * as fs from "fs";
 
 const dotenvConfigFileName = ".env";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const pathname = resolve(__dirname, dotenvConfigFileName);
+const __dirname = path.resolve();
+
+const pathname = path.resolve(__dirname, dotenvConfigFileName);
 
 if (!fs.existsSync(pathname)) {
   throw new Error(`Create a ${dotenvConfigFileName} file and try again!`);
@@ -17,7 +16,7 @@ dotenv.config({ path: dotenvConfigFileName });
 export default {
   client: "pg",
   connection: process.env.DATABASE_URL,
-  pool: { min: 1, max: 1 },
+  pool: { min: 2, max: 10 },
   migrations: {
     name: "migrations",
     directory: "src/shared/database/knex/migrations",
--- src/modules/ping/handler.ts
+++ src/modules/ping/handler.ts
@@ -1,3 +1,23 @@
-export default async function pinghandler(req: any, res: any) {
-  res.send("Pong");
+import { uowRunner } from "@/shared/database/knex";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export default async function pinghandler(
+  req: FastifyRequest,
+  res: FastifyReply,
+) {
+  let isOnline = false;
+
+  try {
+    await uowRunner(async (uow) => {
+      const result = await uow.raw("select true as online");
+      isOnline = result.rows.length > 0;
+    });
+
+    res.status(200).send(`Pong: database is online: ${isOnline}`);
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res
+      .status(500)
+      .send("Unable to acquire a connection or an error occurred.");
+  }
 }
